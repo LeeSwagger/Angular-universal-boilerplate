c955c53c09ff849d6530828c850acb2760ca5ebc
batalygin <batalgin@reksoft.ru>
2020-03-17 16:33:19 +0300

    Добавлен пример подключения кнопки из ui-kit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutes } from './home.routing';
 import { HomeComponent } from './home.component';
+import { YInputModule } from '@yota/ui-kit/input';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutes],
-  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutes, YInputModule],
+  declarations: [HomeComponent]
 })
 export class HomeModule {}
