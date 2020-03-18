363fd5f91519daacae7449d2bbbc5fbc0d51b342
imarkina <irin.markina@mail.ru>
2020-03-18 12:49:27 +0300

    темы

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutes } from './home.routing';
 import { HomeComponent } from './home.component';
 import { DataService } from './data.service';
+import { YLibraryModule } from '@shared/modules/y-library.module';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutes],
+  imports: [CommonModule, HomeRoutes, YLibraryModule],
   declarations: [HomeComponent],
   providers: [DataService]
 })
---
+++ src/app/shared/modules/y-library.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { YButtonModule } from '@yota/ui-kit/button';
+
+@NgModule({
+  imports: [YButtonModule],
+  exports: [YButtonModule]
+})
+export class YLibraryModule {}
